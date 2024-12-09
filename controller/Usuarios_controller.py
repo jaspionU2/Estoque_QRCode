872e67862f433f7567ebb46590da51b32179d3ed
jaspionU2 <gameraleatorio2202@outlook.com>
2024-12-09 01:02:29 -0300

    adicionado service para os usuarios(professores)

--- controller/Usuarios_controller.py
+++ controller/Usuarios_controller.py
@@ -1,36 +1,39 @@
 from fastapi import APIRouter, Response, status  
-from services import Usuarios_service
+from services.Usuarios_service import Aluno_CRUD, Professor_CRUD
+from model.Model_Aluno import Aluno
+from model.Model_Professor import Professor
 
-# class UsuarioService:
-#     def __init__(self):
-#         self.UsuarioService = Usuarios_service
-   
-    
-# usuario = UsuarioService().UsuarioService
+
 router_usuario = APIRouter()  
 
 @router_usuario.get('/getAllProfessores')
 async def get_Professores(res: Response) -> list:
-    professores = [{}] #service.getAllProfessores()
+    professores = await Professor_CRUD.getAllProfessores()
     res.status_code = status.HTTP_200_OK
     return professores
 
 @router_usuario.post('/createProfessor')
-async def create_Professor(new_professor: dict, res: Response) -> None:
-    if new_professor == None:
+async def create_professor(new_professores: list[dict], res: Response):
+    if not new_professores: 
         res.status_code = status.HTTP_400_BAD_REQUEST
-        return None
+        return
+        
+    success = await Professor_CRUD.createProfessor(new_professores)
     
-    # service.createProfessor(new_professor)
-    res.status_code = status.HTTP_201_CREATED
+    if success:
+        res.status_code = status.HTTP_201_CREATED
+        return
+    else:
+        res.status_code = status.HTTP_400_BAD_REQUEST
+        return
 
-@router_usuario.put('/updateProfessor') 
-async def update_Professor(new_values: dict, res: Response) -> None:
+@router_usuario.put('/updateProfessor/{id}') 
+async def update_Professor(id: int, new_values: dict, res: Response) -> None:
     if new_values == None:
         res.status_code = status.HTTP_400_BAD_REQUEST
         return None
     
-    # service.updateProfessor(new_values)
+    await Professor_CRUD.updateProfessor(id, new_values)
     res.status_code = status.HTTP_200_OK
 
 @router_usuario.delete('/deleteProfessor/{id}')
@@ -39,23 +42,29 @@ async def delete(id: int, res: Response) -> None:
         res.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return None
     
-    # sevice.deleteProfessor(id)
+    await Professor_CRUD.deleteProfessor(id)
     res.status_code = status.HTTP_200_OK
 
 @router_usuario.get('/getAllAlunos')
 async def get_alunos(res: Response) -> list:
-    Alunos = await Usuarios_service.getAllAlunos() #service.getAllAlunos()
+    Alunos = await Aluno_CRUD.getAllAlunos() 
     res.status_code = status.HTTP_200_OK
     return Alunos
 
 @router_usuario.post('/createAluno')
-async def create_aluno(new_aluno: dict, res: Response) -> None:
-    if new_aluno == None:
+async def create_aluno(new_aluno: list[Aluno], res: Response) -> None:
+    if not new_aluno :
         res.status_code = status.HTTP_400_BAD_REQUEST
         return None
     
-    await Usuarios_service.createAluno(new_aluno)
-    res.status_code = status.HTTP_201_CREATED
+    sucess = await Aluno_CRUD.createAluno(new_aluno)
+    
+    if sucess:
+        res.status_code = status.HTTP_201_CREATED
+        return
+    else:
+        res.status_code = status.HTTP_400_BAD_REQUEST
+        return
 
 @router_usuario.put('/updateAluno/{id}') 
 async def update_aluno(id: int, new_values: dict, res: Response) -> None:
@@ -63,7 +72,7 @@ async def update_aluno(id: int, new_values: dict, res: Response) -> None:
         res.status_code = status.HTTP_400_BAD_REQUEST
         return None
     
-    await Usuarios_service.updateAluno(id, new_values)
+    await Aluno_CRUD.updateAluno(id, new_values)
     res.status_code = status.HTTP_200_OK
 
 @router_usuario.delete('/deleteAluno/{id}')
@@ -72,5 +81,5 @@ async def delete(id: int, res: Response) -> None:
         res.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return None
     
-    await Usuarios_service.deleteAluno(id)
+    await Aluno_CRUD.deleteAluno(id)
     res.status_code = status.HTTP_200_OK
